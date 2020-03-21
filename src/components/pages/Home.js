6660f4a8c3d9b45b5ca4de5b84259c748df626f1
jezzs2k <jezzs2k@gmail.com>
2020-03-21 09:14:37 +0700

    Fix: not spinner when loading data

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 import Users from '../users/Users';
 import Search from '../users/Search';
+import Spinner from '../layout/Spinner';
+import GithubContext from '../../context/github/githubContext';
 
 const Home = () => {
+  const githubContext = useContext(GithubContext);
+
+  if (githubContext.loading) {
+    return <Spinner />;
+  }
+
   return (
     <Fragment>
       <Search />
